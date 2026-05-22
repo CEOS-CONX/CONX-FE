422e2df3f049ddd4eea51f5f45e148f714c45d2a
yujinnie <yujinnieohh@gmail.com>
2026-05-22 23:13:19 +0900

    feat: search bar 구현

--- conx-fe/src/app/page.tsx
+++ conx-fe/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { SearchBar } from '@/components/common/SearchBar';
+
 export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center">
       <h1 className="text-2xl font-semibold">CONX</h1>
+      <SearchBar />
     </div>
   );
 }
