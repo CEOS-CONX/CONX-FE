46d32bde24efdd4e865dec8eb80b0d4edd55f8df
yeobi <kimhyjjang@gmail.com>
2026-07-31 13:50:28 +0900

    feat: 결과물 제출일 - 모집 마감일, 프로젝트 등록일보다 이르면 에러 노출

--- conx-fe/src/components/project-create/ProjectCreateForm.tsx
+++ conx-fe/src/components/project-create/ProjectCreateForm.tsx
@@ -75,7 +75,7 @@ export default function ProjectCreateForm() {
         if (key === 'projectEndDate' && next.projectStartDate && next.projectEndDate) {
           if (next.projectEndDate.getTime() < next.projectStartDate.getTime()) {
             next.projectEndDate = undefined;
-            setScheduleError('프로젝트 마감일은 시작일 이후여야 합니다.');
+            setScheduleError('프로젝트 마감일은 프로젝트 시작일 이후여야 합니다.');
             setScheduleErrorFields(new Set(['projectEndDate']));
             return next;
           }
@@ -88,12 +88,37 @@ export default function ProjectCreateForm() {
             return next;
           }
         }
+        if (key === 'submissionDate' && next.submissionDate) {
+          if (
+            next.projectStartDate &&
+            next.submissionDate.getTime() < next.projectStartDate.getTime()
+          ) {
+            next.submissionDate = undefined;
+            setScheduleError('결과물 제출일은 프로젝트 시작일 이후여야 합니다.');
+            setScheduleErrorFields(new Set(['submissionDate']));
+            return next;
+          }
+          if (
+            next.recruitDeadline &&
+            next.submissionDate.getTime() < next.recruitDeadline.getTime()
+          ) {
+            next.submissionDate = undefined;
+            setScheduleError('결과물 제출일은 크루 모집 마감일 이후여야 합니다.');
+            setScheduleErrorFields(new Set(['submissionDate']));
+            return next;
+          }
+        }
 
         if ((key === 'brandName' || key === 'managerName' || key === 'email') && next.useMyInfo) {
           next.useMyInfo = false;
         }
 
-        if (key === 'projectStartDate' || key === 'projectEndDate' || key === 'recruitDeadline') {
+        if (
+          key === 'projectStartDate' ||
+          key === 'projectEndDate' ||
+          key === 'recruitDeadline' ||
+          key === 'submissionDate'
+        ) {
           setScheduleErrorFields((prev) => {
             if (prev.size > 0) {
               setScheduleError('');
